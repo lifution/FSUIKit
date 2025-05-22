1079307363ce349bdfe0389b4233107b591391f1
VincentLee <kingofthenorth@foxmail.com>
2025-05-22 16:21:52 +0800

    optimizes codes

--- Source/Classes/Tools/FSRuntime.swift
+++ Source/Classes/Tools/FSRuntime.swift
@@ -17,6 +17,7 @@ import Foundation
 ///
 /// 该方法一般用于特定的场景，字符串为 empty 或 nil 都是表达一样的意思。
 ///
+@inline(__always)
 public func fs_isStringEqualIgnoringEmpty(_ lhs: String?, _ rhs: String?) -> Bool {
     if let l = lhs, l.isEmpty, rhs == nil {
         return true
@@ -35,6 +36,7 @@ public func fs_isStringEqualIgnoringEmpty(_ lhs: String?, _ rhs: String?) -> Boo
 /// 2. 如果两个都是 empty 则返回 false
 /// 3. 如果一个为 nil，另一个为 empty 则同样返回 false
 ///
+@inline(__always)
 public func fs_isStringValueEqual(_ lhs: String?, _ rhs: String?) -> Bool {
     // 只有当 userId 非空时才比较
     let lhsValid = lhs?.isEmpty == false  // 确保 lhs 非空
@@ -48,6 +50,7 @@ public func fs_isStringValueEqual(_ lhs: String?, _ rhs: String?) -> Bool {
 /// 基于当前设备的屏幕缩放倍数，对传进来的 float 数值进行**向上**像素取整。
 /// 例如传进来 "2.1"，在 2x 倍数下会返回 2.5（0.5pt 对应 1px），在 3x 倍数下会返回 2.333（0.333pt 对应 1px）。
 ///
+@inline(__always)
 public func FSFlat<T: FloatingPoint>(_ x: T) -> T {
     guard
         x != T.leastNormalMagnitude,
@@ -64,6 +67,7 @@ public func FSFlat<T: FloatingPoint>(_ x: T) -> T {
 /// 基于当前设备的屏幕缩放倍数，对传进来的 float 数值进行**向下**像素取整。
 /// 例如传进来 "2.6"，在 2x 倍数下会返回 2.5（0.5pt 对应 1px），在 3x 倍数下会返回 2.333（0.333pt 对应 1px）。
 ///
+@inline(__always)
 public func FSFloorFlat<T: FloatingPoint>(_ x: T) -> T {
     guard
         x != T.leastNormalMagnitude,
@@ -79,9 +83,25 @@ public func FSFloorFlat<T: FloatingPoint>(_ x: T) -> T {
 ///
 /// 检查传入的数值是否为有效数值，如果是无效的，则返回 0
 ///
+@inline(__always)
 public func FSRemoveInvalidNumber<T: FloatingPoint>(_ x: T) -> T {
     if x.isNaN || x.isSignalingNaN || x.isInfinite {
-        return 0
+        return T.zero
     }
     return x
+}
+///
+/// 把非法数字转换为正常数字
+/// 如果传入的是非法数字则会返回 `0.0`。
+///
+@inline(__always)
+public func FSRemoveInfinite<T: FloatingPoint>(_ value: T) -> T {
+    guard
+        value.isFinite,
+        !value.isNaN,
+        !value.isSignalingNaN
+    else {
+        return T.zero
+    }
+    return value
 }
